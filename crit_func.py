4eae04c1cd2c32574c0f907073a3d334fced8543
amalsankumar <amalsankumar@gmail.com>
2017-08-17 13:05:32 +0530

    Upto determining attr

--- crit_func.py
+++ crit_func.py
@@ -4,6 +4,7 @@ import csv
 from other_func import *
 
 oth = other_func()
+FUNCS = ['disticnt', 'sum', 'max', 'avg', 'min']
 
 class process:
 
@@ -15,6 +16,10 @@ class process:
         self.conditions = []
         self.tables = []
         self.tables_data = {}
+        self.required_attr = []
+        self.columns = []
+        self.distinct_process = []
+        self.fn_process = []
 
     def process_query(self,query):
         query = oth.format_string(query)
@@ -42,3 +47,44 @@ class process:
             if self.tables[i] not in self.dict.keys():
                 sys.exit('No such table as \'' + self.tables[i] + '\' Exists')
             self.tables_data[self.tables[i]] = oth.read_table_data(self.tables[i])
+
+        self.required_attr = before_from[len('select '):]
+        self.required_attr = oth.format_string(self.required_attr)
+        self.required_attr = self.required_attr.split(',')
+
+        self.process_select()
+        if len(self.columns) + len(self.distinct_process) + len(self.fn_process) < 1:
+            sys.exit('Nothing is given to select')
+
+        if len(self.distinct_process) != 0 and len(self.fn_process) != 0:
+            sys.exit('Distinct and aggregate fns cant be given at the same time')
+        if len(self.clauses) > 1 and len(self.tables) == 1: #where condn on a single tables
+            process_where()
+
+
+
+
+    def process_select(self):
+        column_name = ''
+        for i in self.required_attr:
+            flg = False;
+            i = oth.format_string(i)
+            for fn in FUNCS:
+                if fn + '(' in i.lower():
+                    flg = True
+                    if ')' not in i:
+                        sys.exit('Syntax ERROR: \')\' missing')
+                    else:
+                        column_name = i.strip(')').split(fn + '(')[1]
+                    if fn == 'distinct':
+                        self.distinct_process.append(column_name)
+                    else:
+                        self.fn_process.append([fn, column_name])
+                    break
+            if not flg:
+                i = oth.format_string(i)
+                if i != '':
+                    self.columns.append(i.strip('()'))
+
+
+    # def process_where(self):
